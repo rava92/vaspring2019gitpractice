4298a3c102644d23936f466b17fb28864b325db8
Ravil Abilkadirov <abilkadirov.ra@gmail.com>
2019-07-30 21:19:54 -0400

    initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+
+
+    }
+}
