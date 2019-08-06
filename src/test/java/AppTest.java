2094b3fa4db79adde5e0f8c005cd48d021edb6c8
rava92 <abilkadirov.ra@gmail.com>
2019-08-05 22:36:25 -0400

    Second one

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,6 +4,9 @@ public class AppTest {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello world");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello world");
 
 
 
