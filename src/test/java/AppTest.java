aebdd82f838c477a6e2e626e93a56ceab829649e
Ravil <50155668+rava92@users.noreply.github.com>
2019-08-06 19:32:36 -0400

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,13 +2,7 @@ public class AppTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello world");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello world");
-
-
-
+        System.out.println("Hello World for Rava");
+      
     }
 }
